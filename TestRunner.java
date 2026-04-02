3e7f7229b94461b007640fdd720507fbf18409f5
amarmishra003-QE <amarmishra003@gmail.com>
2026-04-02 15:54:31 +0530

    Create TestRunner.java

---
+++ TestRunner.java
@@ -0,0 +1,12 @@
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "login.feature",
+        glue = "",
+        plugin = {"pretty", "html:target/cucumber-report.html"}
+)
+public class TestRunner {
+}
